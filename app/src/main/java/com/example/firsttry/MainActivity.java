c367f54e29e0ee5ef87568e7c53546d0004b812e
Lakhdar-idrissi-mohammed <aphelay@hotmail.fr>
2021-06-11 16:15:57 +0100

    f2

--- app/src/main/java/com/example/firsttry/MainActivity.java
+++ app/src/main/java/com/example/firsttry/MainActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
@@ -19,7 +21,7 @@ import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
     private InterstitialAd mInterstitialAd;
-    private String TAG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,46 +30,81 @@ public class MainActivity extends AppCompatActivity {
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
+                createPersonalizedAd();
             }
         });
-        AdRequest adRequest = new AdRequest.Builder().build();
+    }
 
-        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+    private void createPersonalizedAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        createInterstitialAd(adRequest);
+    }
+    private void createInterstitialAd(AdRequest adRequest){
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
             @Override
             public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
                 // The mInterstitialAd reference will be null until
                 // an ad is loaded.
                 mInterstitialAd = interstitialAd;
-                Log.i(TAG, "onAdLoaded");
+                Log.d("AdMob", "onAdLoaded");
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        // Called when fullscreen content is dismissed.
+                        Log.d("AdMob", "The ad was dismissed.");
+                        Intent intent = new Intent(MainActivity.this, activity1.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // Called when fullscreen content failed to show.
+                        Log.d("AdMob", "The ad failed to show.");
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        // Called when fullscreen content is shown.
+                        // Make sure to set your reference to null so you don't
+                        // show it a second time.
+                        mInterstitialAd = null;
+                        Log.d("AdMob", "The ad was shown.");
+                    }
+                });
             }
 
             @Override
             public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
                 // Handle the error
-                Log.i(TAG, loadAdError.getMessage());
+                Log.d("....AdMob", loadAdError.getMessage());
                 mInterstitialAd = null;
             }
         });
-
     }
-
+//zbe
     public void guide(View view) {
         switch (view.getId()) {
             case R.id.btn1:
-                Intent intent = new Intent(MainActivity.this, activity1.class);
-                startActivity(intent);
+
                 if (mInterstitialAd != null) {
                     mInterstitialAd.show(MainActivity.this);
+                } else {
+                    Log.d("AdMob", "The interstitial ad wasn't ready yet.");
+                    Intent intent = new Intent(MainActivity.this, activity1.class);
+                    startActivity(intent);
                 }
                 break;
             case R.id.btn2:
-                intent  = new Intent(MainActivity.this, activity1.class);
-                startActivity(intent);
+
                 if (mInterstitialAd != null) {
                     mInterstitialAd.show(MainActivity.this);
+                } else {
+                    Log.d("AdMob", "The interstitial ad wasn't ready yet.");
+                    Intent intent = new Intent(MainActivity.this, activity1.class);
+                    startActivity(intent);
                 }
                 break;
-           
+
         }
     }
 }
