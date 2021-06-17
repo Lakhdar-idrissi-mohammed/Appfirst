b20f2279b0a327d140a78407934ce1f8edfcbbe1
rabiemahdaoui <rabie.mahdaoui@usmba.ac.ma>
2021-06-17 03:44:34 +0100

    Diamond task activity

---
+++ app/src/main/java/com/example/firsttry/DiamondTasks.java
@@ -0,0 +1,72 @@
+package com.example.firsttry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class DiamondTasks extends AppCompatActivity {
+    private int diamondsCount;
+    Button diamondField;
+    Button dd;
+
+    public int getDiamondsCount() {
+        return diamondsCount;
+    }
+
+    public void setDiamondsCount(int diamondsCount) {
+        this.diamondsCount = diamondsCount;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diamond_tasks);
+        diamondsCount=0;
+        diamondField = findViewById(R.id.ddBtn);
+//        dd= (Button) findViewById(R.id.instaBtn);
+//        dd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String url = "https://www.instagram.com/med_lakhdar_idrissi/";
+//
+//                Intent j = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                startActivity(j);
+//            }
+//        });
+
+
+    }
+    public void guide(View view) {
+
+        switch (view.getId()) {
+            case R.id.instaBtn:
+                String url = "https://www.instagram.com/med_lakhdar_idrissi/";
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+                diamondsCount += 800;
+                diamondField.setText(diamondsCount);
+                break;
+            case R.id.ytBtn  :
+                Intent j = new Intent(Intent.ACTION_VIEW);
+                j.setData(Uri.parse("https://www.youtube.com/channel/UCkRa_3MnkiaBa0Su-1KIJBw"));
+                startActivity(j);
+                diamondsCount += 800;
+                diamondField.setText(diamondsCount);
+                break;
+            case R.id.task1:
+                diamondsCount += 300;
+                diamondField.setText(diamondsCount);
+                break;
+            case R.id.task2:
+                diamondsCount += 350;
+                diamondField.setText(diamondsCount);
+                break;
+        }
+    }
+
+}
